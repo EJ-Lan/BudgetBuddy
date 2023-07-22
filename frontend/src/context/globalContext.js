72f57f6a56f29023e90181a73d7840b106019c1a
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-21 19:01:04 -0600

    Add global context to connect to server

---
+++ frontend/src/context/globalContext.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:5000/api/v1/";
+
+const GlobalContext = React.createContext()
+
+export const GlobalProvider = ({children}) => {
+
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    return (
+        <GlobalContext.Provider>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
