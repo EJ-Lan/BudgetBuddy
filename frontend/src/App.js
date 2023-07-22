617092c65a1f9910fb7426c6a131f45b52d26888
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-22 05:48:04 -0600

    Finish Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,21 @@ import Navigation from './Components/Navigation/Navigation'
 import Dashboard from './Components/Dashboard/Dashboard';
 import Income from './Components/Income/Income'
 import Expenses from './Components/Expenses/Expenses';
+import { useGlobalContext } from './context/globalContext';
 
 function App() {
   const [active, setActive] = useState(1)
+
+  const global = useGlobalContext()
+  console.log(global);
 
   const displayData = () => {
     switch(active){
       case 1:
         return <Dashboard />
       case 2:
-        return <Dashboard />
-      case 3:
         return <Income />
-      case 4: 
+      case 3:
         return <Expenses />
       default: 
         return <Dashboard />
